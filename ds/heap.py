c290c05091a48055b547ab1245812aa2669da3cb
jamesc <jamesc.000@gmail.com>
2010-10-26 13:31:17 +0200

    Fixup whitespace.  Fix issue with [] as default arg in method

--- ds/heap.py
+++ ds/heap.py
@@ -1,35 +1,36 @@
 """
 Copyright (c) 2010 CERN. All rights reserved.
 
-Licensed under the Apache License, Version 2.0 (the "License"); 
-you may not use this file except in compliance with the License. 
-You may obtain a copy of the License at 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0 
+    http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software 
-distributed under the License is distributed on an "AS IS" BASIS, 
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
-See the License for the specific language governing permissions and 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
 limitations under the License.
 """
 
 
 class Heap:
     """A heap efficiently supports the following operations:
-    
+
         Insert(e)
         e = ExtractMax()
-        
+
         It maintains the heap invariant, which that the children of any
         element are less that the element."""
-        
-    def __init__(self, contents=[]):
+
+    def __init__(self, contents=None):
         """Create a new, empty Heap.
-        
+
         To make it more pythonic we use a 0-indexed structure"""
-        self._heap = contents
-        
+
+        self._heap = contents or []
+
     def insert(self, elem):
         """Insert an element into the heap, growing it by 1."""
         pos = len(self._heap)
@@ -39,11 +40,11 @@ class Heap:
     def find_max(self):
         """Return the maximum element in the heap"""
         return self._heap[0]
-        
+
     def extract_max(self):
         """Remove and return the maximum element from a heap, maintaining the
         heap invariant."""
-        
+
         lastelt = self._heap.pop() # Raises IndexError on empty heap
         if self._heap:
             returnitem = self._heap[0]
@@ -54,13 +55,15 @@ class Heap:
         return returnitem
 
     def _siftup(self, pos):
-        """given a position in heap, sift that element up to it's correct place"""
+        """given a position in heap, sift that element up to it's
+        correct place"""
         parent = (pos - 1) >>1
-        while pos and self._heap[pos] > self._heap[parent]:    
-            self._heap[pos], self._heap[parent] = self._heap[parent], self._heap[pos]
+        while pos and self._heap[pos] > self._heap[parent]:
+            self._heap[pos], self._heap[parent] \
+                    = self._heap[parent], self._heap[pos]
             pos = (pos - 1) >> 1
             parent = (pos -1) >> 1
-            
+
     def _siftdown(self, startpos, endpos):
         """Given a new element at startpos, sift it down until it
         sits at the correct place"""
@@ -73,13 +76,14 @@ class Heap:
         if right < endpos and self._heap[right] > self._heap[largest]:
             largest = right
         if largest > startpos:
-            self._heap[startpos], self._heap[largest] = self._heap[largest], self._heap[startpos]
+            self._heap[startpos], self._heap[largest] \
+                    = self._heap[largest], self._heap[startpos]
             self._siftdown(largest, endpos)
-        
-        
+
+
     def __len__(self):
-        return self._heap.__len__()    
-        
+        return self._heap.__len__()
+
     def __repr__(self):
         return self._heap.__repr__()
-    
+
